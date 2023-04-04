10fcd94406b2fa175d0254c74f21cfa5c3e9cc44
yudai2929 <yudai201729@icloud.com>
2023-04-04 18:22:10 +0900

    👍fix: header のナビゲーションを修正

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -3,12 +3,22 @@ import React from "react"
 import { ButtonVPrimary } from "../../ui/common/Button/VPrimary"
 import { useRouter } from "next/router"
 import { HEADER_HEIGHT } from "./constants"
+import { pagesPath } from "../../../lib/$path"
 
 export const Header = () => {
+  const router = useRouter()
+  const onClickTop = () => {
+    router.push(pagesPath.$url())
+  }
+  const onClickLogin = () => {
+    router.push(pagesPath.login.$url())
+  }
   return (
     <HStack px={8} py={2} boxShadow="md" height={HEADER_HEIGHT} justifyContent={"space-between"} as={"header"}>
-      <Heading color={"primary"}>Chat App</Heading>
-      <ButtonVPrimary>ログイン</ButtonVPrimary>
+      <Heading color={"primary"} cursor={"pointer"} onClick={onClickTop}>
+        Chat App
+      </Heading>
+      <ButtonVPrimary onClick={onClickLogin}>ログイン</ButtonVPrimary>
     </HStack>
   )
 }
