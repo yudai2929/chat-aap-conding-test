6f52961245db1fcdf66ee56c25e23be4fc679bcd
yudai2929 <yudai201729@icloud.com>
2023-04-05 12:55:38 +0900

    💄feat: メッセージ用のコンポーネントを作成

--- src/mocks/user.ts
+++ src/mocks/user.ts
@@ -3,9 +3,18 @@ import { defaultImageUrl } from "../entities/factories/UserFactory"
 
 export const mockUser: User = {
   userId: "1",
-  name: "test",
+  name: "山田太郎",
   email: "test",
   createdAt: new Date(),
   updatedAt: new Date(),
-  imageProfileUrl: defaultImageUrl,
+  imageProfileUrl: "https://bit.ly/dan-abramov",
+}
+
+export const mockUser2: User = {
+  userId: "2",
+  name: "斉藤花子",
+  email: "test",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+  imageProfileUrl: "https://bit.ly/sage-adebayo",
 }
