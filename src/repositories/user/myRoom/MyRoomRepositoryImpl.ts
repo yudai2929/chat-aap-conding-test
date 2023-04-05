60b9cd4038fa5225192dd0061d272e7cf66d4b3b
yudai2929 <yudai201729@icloud.com>
2023-04-05 10:26:06 +0900

    👍feat: データの永続化

---
+++ src/repositories/user/myRoom/MyRoomRepositoryImpl.ts
@@ -0,0 +1,22 @@
+import { collection, deleteDoc, doc, getDocs, setDoc } from "firebase/firestore"
+import { MyRoomRepository } from "./MyRoomRepository"
+import { db } from "../../../plugins/firebase/client"
+
+class MyRoomRepositoryImpl implements MyRoomRepository {
+  public findRoomIds = async (userId: string): Promise<string[]> => {
+    const userMyRoomRef = collection(db, `users/${userId}/myRooms`)
+    const res = await getDocs(userMyRoomRef)
+    return res.docs.map(doc => doc.id)
+  }
+  public save = async (userId: string, roomId: string): Promise<void> => {
+    console.log(roomId, userId)
+    const userMyRoomRef = doc(db, `users/${userId}/myRooms`, roomId)
+    await setDoc(userMyRoomRef, { roomId })
+  }
+  public delete = async (userId: string, roomId: string): Promise<void> => {
+    const userMyRoomRef = doc(db, `users/${userId}/myRooms`, roomId)
+    await deleteDoc(userMyRoomRef)
+  }
+}
+
+export const myRoomRepository = new MyRoomRepositoryImpl()
